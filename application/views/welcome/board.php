4c6882560395ab6aa4354b4e12e9a7a71b27b973
아트그라피 <root@saegeul.com>
2013-03-20 16:34:45 +0900

    b

--- application/views/welcome/board.php
+++ application/views/welcome/board.php
@@ -1,12 +1,11 @@
 <div class="container">
     <div class="board_header"></div>
 <br>
-	<table class="table table-hover center" style="width: 980px;">
+	<table class="table table-hover center" style="width: 780px;">
 		<thead>
 			<tr class="board_table_header">
 				<th>No</th>
 				<th>Title</th>
-				<th>등록자</th>
 				<th>등록날짜</th>
 			</tr>
 		</thead>
@@ -15,7 +14,6 @@
 			<tr>
 				<td><?=$document->no;?></td>
 				<td><a href="<?=base_url();?>page/getBoard/<?=$document->no;?>"><?=$document->title;?></a></td>
-				<td><?=$document->writer;?></td>
 				<td><?=$document->create_at;?></td>
 			</tr>
 		<?php endforeach ;?>
